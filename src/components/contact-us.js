7b5e1e1d6e1b76e806f265a93c72db5343a7cc44
Etiarse <et.arsenault2000@gmail.com>
2024-10-04 09:12:57 -0400

    emails can now be sent

--- src/components/contact-us.js
+++ src/components/contact-us.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import emailjs from 'emailjs-com';
 
 const validationSchema = Yup.object({
   name: Yup.string().required('Nom est requis'),
@@ -13,84 +14,117 @@ const validationSchema = Yup.object({
   message: Yup.string().required('Message est requis'),
 });
 
-const ContactUs = () => (
-  <div className="contact-us-section">
-    <h2>Contact</h2>
-    <div className="contact-us-content">
-      <div className="contact-us-content-left">
-        <h3>Parlons de votre projet!</h3>
-        <p>
-          Besoin d'aide avec votre projet technologique? Nous pouvons vous
-          aider!
-        </p>
-        <div className="contact-infos">
-          <div className="contact-info">
-            <PhoneIcon></PhoneIcon>
-            <p>+1(418)-930-4988</p>
+const ContactUs = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    object: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.send(
+      'service_6maspdm',           // EmailJS service ID
+      'template_ug3phe3',          // EmailJS template ID
+      formData,                    // Form data
+      'WPG3CZRXZ1sDXegYH'               // EmailJS user ID
+    )
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    })
+    .catch((error) => {
+      console.error('FAILED...', error);
+    });
+
+    setFormData({ name: '', email: '',  object: '', message: '' });
+  };
+
+  return(
+    <div className="contact-us-section">
+      <h2>Contact</h2>
+      <div className="contact-us-content">
+        <div className="contact-us-content-left">
+          <h3>Parlons de votre projet!</h3>
+          <p>
+            Besoin d'aide avec votre projet technologique? Nous pouvons vous
+            aider!
+          </p>
+          <div className="contact-infos">
+            <div className="contact-info">
+              <PhoneIcon></PhoneIcon>
+              <p>+1(418)-930-4988</p>
+            </div>
+            <div className="contact-info">
+              <EmailIcon></EmailIcon>
+              <p>admin@kozzo.ca</p>
+            </div>
           </div>
-          <div className="contact-info">
-            <EmailIcon></EmailIcon>
-            <p>admin@kozzo.ca</p>
+          <p>Restons connectés</p>
+          <div className="social-medias">
+            <LinkedInIcon></LinkedInIcon>
+            <InstagramIcon></InstagramIcon>
           </div>
         </div>
-        <p>Restons connectés</p>
-        <div className="social-medias">
-          <LinkedInIcon></LinkedInIcon>
-          <InstagramIcon></InstagramIcon>
-        </div>
-      </div>
-      <div className="contact-us-content-right">
-        <h3>Comment pouvons-nous vous aider?</h3>
-        <Formik
-          initialValues={{ name: '', email: '', subject: '', message: '' }}
-          validationSchema={validationSchema}
-          onSubmit={(values) => {
-            alert(JSON.stringify(values, null, 2));
-          }}
-        >
-          {() => (
-            <Form>
-              <div className="client-personal-info">
-                <div className="form-group">
-                  <Field name="name" type="text" placeholder="Nom" />
-                  <ErrorMessage name="name" component="div" className="error" />
+        <div className="contact-us-content-right">
+          <h3>Comment pouvons-nous vous aider?</h3>
+          <Formik
+            initialValues={{ name: '', email: '', subject: '', message: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+              alert(JSON.stringify(values, null, 2));
+            }}
+          >
+            {() => (
+              <Form onSubmit={handleSubmit}>
+                <div className="client-personal-info">
+                  <div className="form-group">
+                    <Field name="name" type="text" placeholder="Nom" value={formData.name} onChange={handleChange} />
+                    <ErrorMessage name="name" component="div" className="error" />
+                  </div>
+
+                  <div className="form-group">
+                    <Field name="email" type="email" placeholder="Email" value={formData.email} onChange={handleChange}/>
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="error"
+                    />
+                  </div>
                 </div>
 
-                <div className="form-group">
-                  <Field name="email" type="email" placeholder="Email" />
+                <div className="form-group client-request-info">
+                  <Field name="object" type="text" placeholder="Sujet" value={formData.object} onChange={handleChange}/>
                   <ErrorMessage
-                    name="email"
+                    name="object"
                     component="div"
                     className="error"
                   />
                 </div>
-              </div>
 
-              <div className="form-group client-request-info">
-                <Field name="subject" type="text" placeholder="Sujet" />
-                <ErrorMessage
-                  name="subject"
-                  component="div"
-                  className="error"
-                />
-              </div>
+                <div className="form-group client-request-info">
+                  <Field name="message" as="textarea" placeholder="Message" value={formData.message} onChange={handleChange}/>
+                  <ErrorMessage
+                    name="message"
+                    component="div"
+                    className="error"
+                  />
+                </div>
 
-              <div className="form-group client-request-info">
-                <Field name="message" as="textarea" placeholder="Message" />
-                <ErrorMessage
-                  name="message"
-                  component="div"
-                  className="error"
-                />
-              </div>
-
-              <button type="submit">Envoyer</button>
-            </Form>
-          )}
-        </Formik>
+                <button type="submit">Envoyer</button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default ContactUs;
