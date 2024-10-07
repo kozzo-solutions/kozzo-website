450701920b7a6f865f4fee2077c1bc9ac562aa25
Etiarse <et.arsenault2000@gmail.com>
2024-10-07 16:28:25 -0400

    try fr default language

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from './components/about-us';
 import Footer from './components/footer';
 import ContactUs from './components/contact-us';
 import { Stack } from '@mui/material';
+import "./i18n"
 
 function App() { 
   return (
