cacc3cd5a21936dfd1c7bc94172568ee6bfb0605
Etiarse <et.arsenault2000@gmail.com>
2024-10-07 11:42:04 -0400

    fix import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import './components/button/button.css';
 import Header from './components/header';
 import TopSection from './components/top-section';
 import Services from './components/services';
