44b89da810dc4f38cc72d90e36f41f20d2de4b6a
JoBess7 <JOBES10@ulaval.ca>
2024-11-04 16:35:15 -0500

    add img thumbnail

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
+import heroImage from './resources/images/hero-image-space.png';
 
 import common_fr from './resources/translations/fr/common.json';
 import common_en from './resources/translations/en/common.json';
@@ -23,6 +24,7 @@ i18next.init({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link rel="image_src" href={heroImage} />
     <I18nextProvider i18n={i18next}>
       <App />
     </I18nextProvider>
